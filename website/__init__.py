f0504285513aa7c105a5d7e48cf043eea190bf52
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2022-02-24 21:22:55 +0000

    add db to init file

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'im only 19 but my mind is old' #encrpy session/data cookies for website
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'squilte:///{DB_NAME}'
+    db.init_app(app)
+    
     
     from .views import views
     from .auth import auth
